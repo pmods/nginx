dccdfbc576c5e15063a5deb97cd28da5bc9ff8db
Andrew Marks <amracks@gmail.com>
2014-07-19 07:49:18 -0700

    Added server cert installation

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,5 +1,8 @@
 class nginx (
-    $nginxconfsrc = '/root/etc/nginx/nginx.conf'
+    $nginxconfsrc = '/root/etc/nginx/nginx.conf',
+    $sslcert      = '/root/etc/nginx/ssl-cert.pem',
+    $sslkey       = '/root/etc/nginx/ssl-key.pem',
+    $sslca        = '/root/etc/nginx/ssl-ca.crt',
 ){
 
     $nginxconfdir = '/usr/local/etc/nginx/conf.d'
@@ -40,6 +43,36 @@ class nginx (
         ensure => directory,
         owner  => 'root',
         group  => 'wheel',
+    }
+
+    file { 'nginx-cert':
+        path    => '/usr/local/etc/nginx/ssl-cert.pem',
+        ensure  => file,
+        owner   => 'root',
+        group   => 'wheel',
+        source  => $sslcert,
+        require => Package[$pkgname],
+        notify  => Service[$servicename]
+    }
+
+    file { 'nginx-key':
+        path    => '/usr/local/etc/nginx/ssl-key.pem',
+        ensure  => file,
+        owner   => 'root',
+        group   => 'wheel',
+        source  => $sslkey,
+        require => Package[$pkgname],
+        notify  => Service[$servicename]
+    }
+
+    file { 'nginx-ca':
+        path    => '/usr/local/etc/nginx/ssl-ca.crt',
+        ensure  => file,
+        owner   => 'root',
+        group   => 'wheel',
+        source  => $sslca,
+        require => Package[$pkgname],
+        notify  => Service[$servicename]
     }
 
     service { $servicename:
